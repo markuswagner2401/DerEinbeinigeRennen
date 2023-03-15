b7d9431fcd845e9091ebfc38b41129b652ffe37c
markuswagner2401 <markuswagner2401@googlemail.com>
2023-03-15 11:25:28 +0100

    seed peak tracker neu programmiert

--- Assets/00_Crash/Scripts/VRMovement/SpeedPeakTracker.cs
+++ Assets/00_Crash/Scripts/VRMovement/SpeedPeakTracker.cs
@@ -23,11 +23,39 @@ namespace ObliqueSenastions.VRRigSpace
 
         [SerializeField] float speedPeak;
 
-        [SerializeField] float smoothing = 0.1f;
+        [SerializeField] float peakDetectorSmoothing = 0.1f;
+
+        [SerializeField] float outputRisingSmoothing = 0.1f;
+
+        [SerializeField] float outputFallingSmoothing = 0.1f;
+
+        [SerializeField] float fallingDuration = 3f;
+
+        [SerializeField] AnimationCurve fallingCurve;
+
+        [SerializeField] bool dynamicNormaization = true;
+
+        [SerializeField] float minSpeed = 0f;
+        [SerializeField] float maxSpeed = 10f;
+
+        float highestSpeed = 0.1f;
+
+        float risingTimer = 0;
+
+        float fallingTimer = 0;
+
+        [SerializeField] float outputValue;
+
+
+
+
+
+        
+
         void Start()
         {
             currentSpeed = simpleVelocityTracker.GetLocalSpeed();
-            smoothedSpeed = Mathf.Lerp(currentSpeed, simpleVelocityTracker.GetLocalSpeed(), smoothing);
+            smoothedSpeed = Mathf.Lerp(currentSpeed, simpleVelocityTracker.GetLocalSpeed(), peakDetectorSmoothing);
 
             lastSpeed = currentSpeed;
             lastSmoothedSpeed = smoothedSpeed;
@@ -38,35 +66,75 @@ namespace ObliqueSenastions.VRRigSpace
         {
 
             currentSpeed = simpleVelocityTracker.GetLocalSpeed();
-            smoothedSpeed = Mathf.Lerp(smoothedSpeed, currentSpeed, smoothing);
+            smoothedSpeed = Mathf.Lerp(smoothedSpeed, currentSpeed, peakDetectorSmoothing);
+
+            highestSpeed = (currentSpeed > highestSpeed) ? currentSpeed : highestSpeed;
 
             if (lastSmoothedSpeed < smoothedSpeed)
-                {
-                    rising = true;
-                    
-                    speedPeak = (capturedSpeedPeak > currentSpeed) ? capturedSpeedPeak : currentSpeed;
-                    capturedSpeedPeak = speedPeak;
-                }
+            {
+                rising = true;
+                speedPeak = (capturedSpeedPeak > currentSpeed) ? capturedSpeedPeak : currentSpeed;
+                capturedSpeedPeak = speedPeak;
 
-                else
-                {
-                    rising = false;
-                    capturedSpeedPeak = 0f;
-                }
 
-            // if (currentSpeed > speedThreshold)
-            // {
-                
+            }
 
-            // }
+            else
+            {
+                rising = false;
+                capturedSpeedPeak = 0f;
+
+
+            }
 
 
             lastSpeed = currentSpeed;
             lastSmoothedSpeed = smoothedSpeed;
 
+            ///
+
+            if (rising)
+            {
+                fallingTimer = 0;
+                risingTimer += Time.deltaTime;
+                outputValue = Mathf.Lerp(outputValue, speedPeak, outputRisingSmoothing);
+            }
+
+            else
+            {
+                risingTimer = 0;
+                fallingTimer += Time.deltaTime;
+
+                float valueOnCurve = Mathf.Lerp(speedPeak, 0f, fallingCurve.Evaluate(fallingTimer / fallingDuration));
+                outputValue = Mathf.Lerp(outputValue, valueOnCurve, outputFallingSmoothing);
+
+            }
+
+
+        }
+
+        public float GetOutputValueNormalized()
+        {
+            if(dynamicNormaization)
+            {
+                return Mathf.InverseLerp(0, highestSpeed, outputValue);
+            }
+
+            else
+            {
+                return Mathf.InverseLerp(minSpeed, maxSpeed, outputValue);
+            }
             
         }
+
+        private void OnDrawGizmos() 
+        {
+            
+            Gizmos.DrawLine(transform.position, transform.position + transform.up * GetOutputValueNormalized() * 10f);
+        }
     }
+
+
 
 }
 
