118485c3bb3b32513958fa39c92fdcce9aba66e1
MW2401 <markuswagner2401@googlemail.com>
2023-04-12 13:19:16 +0200

    custom motor controller sript weiterentwickelt

--- Assets/00_Crash/Scripts/Debug/CustomCarSoundController.cs
+++ Assets/00_Crash/Scripts/Debug/CustomCarSoundController.cs
@@ -2,63 +2,107 @@ using UnityEngine;
 using ObliqueSenastions.VRRigSpace;
 using System.Collections;
 
+
 public class CustomCarSoundController : MonoBehaviour
 {
+    [System.Serializable]
+    public struct Gear
+    {
+        public AudioClip gearSound;
+        public float minPitch;
+        public float maxPitch;
+        public float startVolume;
+        [Range(0, 1)] public float gearSwitch;
+    }
+
+    [SerializeField] float volume = 1f;
+
     public AudioSource audioSource;
     public AudioSource gearSwitchAudioSource;
 
-    [SerializeField] SimpleVelocityTracker simpleVelocityTracker;
-    public AudioClip lowGearSound;
-    public AudioClip midGearSound;
-    public AudioClip highGearSound;
+    [SerializeField][Range(0, 1)] float inputValue;
     public AudioClip gearSwitchSound;
-
-    public float minPitch = 0.5f;
-    public float maxPitch = 2.0f;
     public float minVelocity = 0.0f;
     public float maxVelocity = 30.0f;
 
-    public float midGearThreshold = 10.0f;
-    public float highGearThreshold = 20.0f;
+    [SerializeField] private Gear[] gears;
 
     public float pitchSmoothing = 2.0f;
     public float gearSwitchDuration = 0.3f;
-    public float gearSwitchPitchOffset = 0.5f;
+    public float volumeFadeDuration = 0.1f;
+
+    public bool motorRunning = true;
+    public int fixGear = -1;
 
     private float currentPitch;
     private int currentGear = 0;
     private bool isSwitchingGears = false;
 
+    private void Start()
+    {
+        if (motorRunning)
+        {
+            audioSource.clip = gears[0].gearSound;
+            audioSource.Play();
+        }
+    }
+
     private void Update()
     {
+        HandleMotorRunning();
+
+        if (!motorRunning) return;
+
         if (isSwitchingGears) return;
 
-        float currentVelocity = simpleVelocityTracker.GetSpeed();
+        float currentVelocity = inputValue;
         float normalizedVelocity = Mathf.InverseLerp(minVelocity, maxVelocity, currentVelocity);
-        float targetPitch = Mathf.Lerp(minPitch, maxPitch, normalizedVelocity);
 
         int targetGear = 0;
-        AudioClip targetClip = lowGearSound;
+        AudioClip targetClip = gears[0].gearSound;
 
-        if (currentVelocity < midGearThreshold)
+        if (fixGear >= 0)
         {
-            targetGear = 1;
-            targetClip = lowGearSound;
-        }
-        else if (currentVelocity >= midGearThreshold && currentVelocity < highGearThreshold)
-        {
-            targetGear = 2;
-            targetClip = midGearSound;
+            if (fixGear >= gears.Length)
+            {
+                targetGear = gears.Length - 1;
+            }
+            else
+            {
+                targetGear = fixGear;
+            }
+            targetClip = gears[targetGear].gearSound;
         }
         else
         {
-            targetGear = 3;
-            targetClip = highGearSound;
+            for (int i = 0; i < gears.Length - 1; i++)
+            {
+                float switchPoint = Mathf.Lerp(minVelocity, maxVelocity, gears[i].gearSwitch);
+                float nextSwitchPoint = Mathf.Lerp(minVelocity, maxVelocity, gears[i + 1].gearSwitch);
+
+                if (currentVelocity >= switchPoint && currentVelocity < nextSwitchPoint)
+                {
+                    targetGear = i;
+                    targetClip = gears[i].gearSound;
+                    break;
+                }
+                if (nextSwitchPoint <= switchPoint)
+                {
+                    Debug.LogWarning("Gear[" + (i + 1) + "] has a smaller gearSwitch value than Gear[" + i + "]. Ignoring Gear[" + (i + 1) + "].");
+                }
+            }
+            if (currentVelocity >= Mathf.Lerp(minVelocity, maxVelocity, gears[gears.Length - 1].gearSwitch))
+            {
+                targetGear = gears.Length - 1;
+                targetClip = gears[gears.Length - 1].gearSound;
+            }
         }
 
-        if (currentGear != targetGear)
+        float targetPitch = Mathf.Lerp(gears[targetGear].minPitch, gears[targetGear].maxPitch, normalizedVelocity);
+
+        if (currentGear != targetGear && fixGear == -1)
         {
-            StartCoroutine(SwitchGears(targetClip));
+            StartCoroutine(SwitchGears(targetGear, targetClip));
             currentGear = targetGear;
         }
         else
@@ -70,23 +114,72 @@ public class CustomCarSoundController : MonoBehaviour
 
             currentPitch = Mathf.Lerp(currentPitch, targetPitch, Time.deltaTime * pitchSmoothing);
             audioSource.pitch = currentPitch;
+
+        }
+
+    }
+
+    private void HandleMotorRunning()
+    {
+        if (motorRunning)
+        {
+            if (!audioSource.isPlaying)
+            {
+                audioSource.clip = gears[0].gearSound;
+                audioSource.Play();
+            }
+            if (audioSource.volume < volume)
+            {
+                audioSource.volume += Time.deltaTime / volumeFadeDuration;
+            }
+        }
+        else
+        {
+            if (audioSource.volume > 0)
+            {
+                audioSource.volume -= Time.deltaTime / volumeFadeDuration;
+            }
+            else
+            {
+                audioSource.Stop();
+            }
         }
     }
 
-    private IEnumerator SwitchGears(AudioClip targetClip)
+    public void StartMotor(bool value)
+    {
+        motorRunning = value;
+    }
+
+    private IEnumerator SwitchGears(int targetGear, AudioClip targetClip)
     {
         isSwitchingGears = true;
-        audioSource.Stop();
         gearSwitchAudioSource.clip = gearSwitchSound;
         gearSwitchAudioSource.Play();
 
-        yield return new WaitForSeconds(gearSwitchDuration);
+        float transitionStartTime = Time.time;
+        float initialPitch = currentPitch;
+        float targetMinPitch = gears[targetGear].minPitch;
 
-        currentPitch -= gearSwitchPitchOffset;
-        currentPitch = Mathf.Clamp(currentPitch, minPitch, maxPitch);
+        while (Time.time - transitionStartTime < gearSwitchDuration)
+        {
+            currentPitch = Mathf.Lerp(initialPitch, targetMinPitch, (Time.time - transitionStartTime) / gearSwitchDuration);
+            audioSource.pitch = currentPitch;
+            yield return null;
+        }
 
+        audioSource.Stop();
         audioSource.clip = targetClip;
         audioSource.Play();
+
+        // Fade in the volume after the gear change
+        audioSource.volume = gears[targetGear].startVolume;
+        float volumeFadeStartTime = Time.time;
+        while (Time.time - volumeFadeStartTime < volumeFadeDuration)
+        {
+            audioSource.volume = Mathf.Lerp(gears[targetGear].startVolume, volume, (Time.time - volumeFadeStartTime) / volumeFadeDuration);
+            yield return null;
+        }
 
         isSwitchingGears = false;
     }
